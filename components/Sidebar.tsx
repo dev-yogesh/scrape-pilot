57633e739543f4d9e46a1d3c9d4b351579f8dee4
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-04 08:31:46 +0530

    add: sidebar and breadcrumb

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,9 @@ import {
 } from "lucide-react";
 import React from "react";
 import Logo from "@/components/Logo";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { buttonVariants } from "./ui/button";
 
 const routes = [
   {
@@ -33,10 +36,32 @@ const routes = [
 ];
 
 function DesktopSidebar() {
+  const pathname = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathname.includes(route.href)
+    ) || routes[0];
   return (
     <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
       <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
         <Logo />
+      </div>
+      <div className="p-2">TODO CREDITS</div>
+      <div className="flex flex-col p-2">
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={buttonVariants({
+              variant:
+                activeRoute.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+          >
+            <route.icon size={20} /> {route.label}
+          </Link>
+        ))}
       </div>
     </div>
   );
