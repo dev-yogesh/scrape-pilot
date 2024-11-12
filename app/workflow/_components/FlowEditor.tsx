86aafd10f5518bc685c825d75d425cf7cb55fac0
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-12 21:39:00 +0530

    add: node header and settings options

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -19,6 +19,9 @@ const nodeTypes = {
   FlowScrapeNode: NodeComponent,
 };
 
+const snapGrid: [number, number] = [50, 50];
+const fitViewOptions = { padding: 1 };
+
 function FlowEditor({ workflow }: { workflow: Workflow }) {
   const [nodes, setNodes, onNodeChange] = useNodesState([
     CreateFlowNode(TaskType.LAUNCH_BROWSER),
@@ -32,8 +35,12 @@ function FlowEditor({ workflow }: { workflow: Workflow }) {
         onEdgesChange={onEdgesChange}
         onNodesChange={onNodeChange}
         nodeTypes={nodeTypes}
+        snapToGrid
+        snapGrid={snapGrid}
+        fitViewOptions={fitViewOptions}
+        fitView
       >
-        <Controls position="top-left" />
+        <Controls position="top-left" fitViewOptions={fitViewOptions} />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
       </ReactFlow>
     </main>
