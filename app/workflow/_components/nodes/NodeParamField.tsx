46298a5f9c3e554a72f7667ede6cebd1f09d0b6d
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-12 22:01:18 +0530

    wip: node inputs

---
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { TaskParam } from "@/types/task";
+
+function NodeParamField({ param }: { param: TaskParam }) {
+  return <div>NodeParamField</div>;
+}
+
+export default NodeParamField;
