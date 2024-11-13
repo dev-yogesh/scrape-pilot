dd43f230dc6df86d61994cd3b01fc79f621bd0c4
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-13 21:41:50 +0530

    add: workflow node input

--- app/workflow/_components/nodes/NodeParamField.tsx
+++ app/workflow/_components/nodes/NodeParamField.tsx
@@ -1,9 +1,50 @@
 "use client";
 
-import { TaskParam } from "@/types/task";
+import { TaskParam, TaskParamType } from "@/types/task";
+import StringParam from "./param/StringParam";
+import { useReactFlow } from "@xyflow/react";
+import { AppNode } from "@/types/appNode";
+import { useCallback } from "react";
 
-function NodeParamField({ param }: { param: TaskParam }) {
-  return <div>NodeParamField</div>;
+function NodeParamField({
+  param,
+  nodeId,
+}: {
+  param: TaskParam;
+  nodeId: string;
+}) {
+  const { updateNodeData, getNode } = useReactFlow();
+  const node = getNode(nodeId) as AppNode;
+  const value = node?.data?.inputs?.[param.name];
+
+  const updateNodeParamValue = useCallback(
+    (newVal: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data?.inputs,
+          [param.name]: newVal,
+        },
+      });
+    },
+    [nodeId, updateNodeData, param.name, node?.data?.inputs]
+  );
+
+  switch (param.type) {
+    case TaskParamType.STRING:
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      );
+    default:
+      return (
+        <div className="w-full">
+          <p className="text-xs text-muted-foreground">Not implemented</p>
+        </div>
+      );
+  }
 }
 
 export default NodeParamField;
