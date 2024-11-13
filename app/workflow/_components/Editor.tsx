445fbd5270b1392f4b239dc971244ea8120cc752
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-13 21:55:32 +0530

    wip: editor topbar

--- app/workflow/_components/Editor.tsx
+++ app/workflow/_components/Editor.tsx
@@ -3,11 +3,13 @@
 import { Workflow } from "@prisma/client";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "./FlowEditor";
+import Topbar from "./topbar/Topbar";
 
 function Editor({ workflow }: { workflow: Workflow }) {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
+        <Topbar title="Workflow editor" subtitle="text" />
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
