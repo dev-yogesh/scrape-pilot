629b8b6ad5fdcb8f656ed6ac54002a3b7ff733ef
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-16 11:03:40 +0530

    add: node output

--- lib/workflow/task/PageToHtml.tsx
+++ lib/workflow/task/PageToHtml.tsx
@@ -15,4 +15,14 @@ export const PageToHtmlTask = {
       required: true,
     },
   ],
+  outputs: [
+    {
+      name: "Html",
+      type: TaskParamType.STRING,
+    },
+    {
+      name: "Web page",
+      type: TaskParamType.BROWSER_INSTANCE,
+    },
+  ],
 };
