7e81ef426193dc3817355a0fe940332eb234ceb5
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-19 22:07:09 +0530

    wip: execution plan

--- lib/workflow/executePlan.ts
+++ lib/workflow/executePlan.ts
@@ -1,6 +1,10 @@
 import { AppNode } from "@/types/appNode";
-import { WorkflowExecutionPlan } from "@/types/workflow";
-import { Edge } from "@xyflow/react";
+import {
+  WorkflowExecutionPlan,
+  WorkflowExecutionPlanPhase,
+} from "@/types/workflow";
+import { Edge, getIncomers } from "@xyflow/react";
+import { TaskRegistry } from "./task/registry";
 
 type FlowToExecutionPlanType = {
   executionPlan?: WorkflowExecutionPlan;
@@ -10,6 +14,55 @@ export function FlowToExecutionPlan(
   nodes: AppNode[],
   edges: Edge[]
 ): FlowToExecutionPlanType {
-  const executionPlan: WorkflowExecutionPlan = [];
+  const entryPoint = nodes.find(
+    (node) => TaskRegistry[node.data.type].isEntryPoint
+  );
+
+  if (!entryPoint) {
+    throw new Error("TODO: HANDLE THIS ERROR");
+  }
+
+  const planned = new Set<string>();
+  const executionPlan: WorkflowExecutionPlan = [
+    {
+      phase: 1,
+      nodes: [entryPoint],
+    },
+  ];
+
+  for (
+    let phase = 2;
+    phase <= nodes.length || planned.size < nodes.length;
+    phase++
+  ) {
+    const nextPhase: WorkflowExecutionPlanPhase = { phase, nodes: [] };
+    for (const currentNode of nodes) {
+      if (planned.has(currentNode.id)) {
+        // Node already put in the execution plan.
+        continue;
+      }
+
+      const invalidInputs = getInvalidInputs(currentNode, edges, planned);
+      if (invalidInputs.length > 0) {
+        const incomers = getIncomers(currentNode, nodes, edges);
+        if (incomers.every((incomer) => planned.has(incomer.id))) {
+          // If all incoming incomers/edges are planned and there are still invalid inputs
+          // this means that this perticular node has an invalid inputs
+          // which means that the workflow is invalid
+          console.error("invalid inputs", currentNode.id, invalidInputs);
+          throw new Error("TODO: HANDLE ERROR 1");
+        } else {
+          // let's skip this node for now
+          continue;
+        }
+      }
+
+      nextPhase.nodes.push(currentNode);
+      planned.add(currentNode.id);
+    }
+  }
+
   return { executionPlan };
 }
+
+function getInvalidInputs(node: AppNode, edges: Edge[], planned: Set<string>) {}
