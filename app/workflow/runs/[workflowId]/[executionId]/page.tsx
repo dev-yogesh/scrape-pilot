60329be69e476a919390afd67132ca1bab001286
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-11-27 03:06:30 +0530

    add: workflow run page

---
+++ app/workflow/runs/[workflowId]/[executionId]/page.tsx
@@ -0,0 +1,50 @@
+import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExectionWithPhase";
+import Topbar from "@/app/workflow/_components/topbar/Topbar";
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
+
+function ExecutionViewerPage({
+  params,
+}: {
+  params: {
+    executionId: string;
+    workflowId: string;
+  };
+}) {
+  return (
+    <div className="flex flex-col h-screen w-full overflow-hidden">
+      <Topbar
+        workflowId={params.workflowId}
+        title="Workflow run details"
+        subtitle={`Run ID: ${params.executionId}`}
+        hideButtons
+      />
+      <section className="flex h-full overflow-auto">
+        <Suspense
+          fallback={
+            <div className="flex w-full items-center justify-center">
+              <Loader2Icon className="h-10 w-10 animate-spin stroke-primary" />
+            </div>
+          }
+        >
+          <ExecutionViewerWrapper executionId={params.executionId} />
+        </Suspense>
+      </section>
+    </div>
+  );
+}
+
+export default ExecutionViewerPage;
+
+async function ExecutionViewerWrapper({
+  executionId,
+}: {
+  executionId: string;
+}) {
+  const workflowExection = await GetWorkflowExecutionWithPhases(executionId);
+  if (!workflowExection) {
+    return <div>Not found</div>;
+  }
+
+  return <pre>{JSON.stringify(workflowExection, null, 4)}</pre>;
+}
