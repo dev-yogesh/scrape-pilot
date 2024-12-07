feb512f948ce9148e40a8a816d25a5db425ac540
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-12-07 08:13:33 +0530

    wip: execution viewer

--- lib/workflow/executor/LaunchBrowserExecutor.ts
+++ lib/workflow/executor/LaunchBrowserExecutor.ts
@@ -9,7 +9,7 @@ export async function LaunchBrowserExecutor(
   try {
     const websiteUrl = environment.getInput("Website Url");
     const browser = await puppeteer.launch({
-      headless: false, // for testing
+      headless: true, // for testing
     });
     environment.setBrowser(browser);
     const page = await browser.newPage();
