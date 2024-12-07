0d4de289acf1f50c9c2cadd79a7a8b5f0bf7c8f3
yogeshtriveni.g <yogeshtrivnei.g@digitral.com>
2024-12-07 18:27:39 +0530

    add: user balance

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button, buttonVariants } from "./ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import UserAvailableCreditsBadge from "./UserAvailableCreditsBadge";
 
 const routes = [
   {
@@ -48,7 +49,9 @@ function DesktopSidebar() {
       <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
         <Logo />
       </div>
-      <div className="p-2">TODO CREDITS</div>
+      <div className="p-2">
+        <UserAvailableCreditsBadge />
+      </div>
       <div className="flex flex-col p-2 gap-1">
         {routes.map((route) => (
           <Link
